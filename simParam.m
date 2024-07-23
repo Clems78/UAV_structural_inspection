ff01d8104ca7684db8792488a4d8b554b4fad14b
Clément <clement.laguerre.972@cranfield.ac.uk>
2024-07-23 00:54:12 +0000

    add filtering options and position of the camera with regards to the center of mass of the drone

--- simParam.m
+++ simParam.m
@@ -9,7 +9,7 @@ sensor_height = 23.8; % camera frame height (mm)
 Iw = 9504 ; % Width resoluwidwition (pixel)
 Ih = 6336; % Height resolution (pixel)
 f = 50; % Focal lenght (mm)
-camera_location = [120 120 200]; % Distance from the center of the drone (mm)
+camera_location = [-30 120 229]; % Distance from the center of mass of the drone to the camera (mm)
 
 % UAV specification
 CAuav = 1; % sphere radius for collision avoidance (m)
@@ -33,7 +33,7 @@ b = 200; % semi-axes lenght b (mm) minor axis // to surface
 c = 200; % semi-axes lenght c (mm) axis perpendicular to surface
 
 % Detection parameters
-alpha_t = 5; % max allowable angle between the camera's optical axis and the surface normal (degree)
+alpha_t = 20; % max allowable angle between the camera's optical axis and the surface normal (degree)
 G = 0.3; % Ground Sampling Distance (mm/pixel)
 d_insp = G * f * Ih / sensor_height; % inspection distance / camera size and resolution should be the shortest distance between height and width
 

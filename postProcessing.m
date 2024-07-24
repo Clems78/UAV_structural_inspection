08cfce8332f0452196da298f6e9c2b6833cbf617
Clément <clement.laguerre.972@cranfield.ac.uk>
2024-07-24 16:40:18 +0000

    works ray tracing

--- postProcessing.m
+++ postProcessing.m
@@ -1,8 +1,19 @@
+clc;
+simParam;
+
 % Given quaternion components
-q_x = -0.012667;
-q_y = -0.00102;
-q_z = 0.006364;
-q_w = 0.999899;
+% q_x = -0.000132;
+% q_y = 0.000398;
+% q_z = -0.004731;
+% q_w = 0.999899;
+% -0.000132	0.000398	-0.004731	0.999989
+
+q_x = -0.016629;
+q_y = -0.010365;
+q_z = -0.00338;
+q_w =  0.999802;
+
+
 
 % Position of the drone when it took the picture in metres
 x_pp = 1.122386; 
@@ -33,6 +44,30 @@ disp(heading_transported);
 heading_deg = rad2deg(heading_transported);
 disp('Heading (yaw) in degrees:');
 disp(heading_deg);
+
+% Extract the pitch angle
+pitch = euler(2);
+
+% Display the result
+disp('Pitch angle in radians:');
+disp(pitch);
+
+% Optionally, convert to degrees if needed
+pitch_deg = rad2deg(pitch);
+disp('Pitch angle in degrees:');
+disp(pitch_deg);
+
+% Extract the roll angle
+roll = euler(3);
+
+% Display the result
+disp('Roll angle in radians:');
+disp(roll);
+
+% Optionally, convert to degrees if needed
+roll_deg = rad2deg(roll);
+disp('Roll angle in degrees:');
+disp(roll_deg);
 
 % GT viewpoints
 gt_viewpoints = [camera_location_gt, heading_deg];
